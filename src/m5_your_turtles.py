6e525f65b661d753791cdb8c5311b7ed3f1f3efa
oconnejc <oconnejc@rose-hulman.edu>
2019-05-22 10:16:37 -0400

    first commit

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Joe O'Connell.
 """
 ########################################################################
-# TODO: 1.
+# done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,26 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window =rg.TurtleWindow()
+greg=rg.SimpleTurtle('turtle')
+stacy=rg.SimpleTurtle('turtle')
+
+for k in range(40):
+    greg.pen = rg.Pen('red', 5)
+    greg.speed=50
+    greg.draw_square(k+150)
+    greg.pen_up()
+    greg.right(60)
+    greg.forward(3)
+    greg.left(60)
+    greg.pen_down()
+    stacy.pen=rg.Pen('pink',3)
+    stacy.speed=70
+    stacy.draw_circle(50)
+    stacy.pen_up()
+    stacy.right(180)
+    stacy.forward(3)
+    stacy.right(180)
+    stacy.pen_down()
+window.close_on_mouse_click()
